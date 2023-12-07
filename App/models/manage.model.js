07e9fddf698abeba2ed62d03813e55cf6047e44e
MarkStanly <nhien.nguyenroad2mag80@hcmut.edu.vn>
2023-12-07 09:24:25 +0700

    added update

--- App/models/manage.model.js
+++ App/models/manage.model.js
@@ -20,11 +20,11 @@ class ManageModel {
     db.query("INSERT INTO printer SET ?", newPrinter, (err, res) => {
         if (err) {
             console.log("Error when adding printer: ", err);
-            result(err, null);
+            result(null);
             return;
         }
         console.log("Added printer :", { id: res.printer_id, ...newPrinter });
-        result(null, newAccount);
+        result(res);
     });
   }
 
@@ -33,11 +33,11 @@ class ManageModel {
     db.query(`DELETE FROM printer WHERE printer_id = '${printerID}'`, (err, res) => {
         if (err) {
             console.log("Error when deleting printer: ", err);
-            result(err, null);
+            result(null);
             return;
         }
         console.log("Deleted printer with ID:"+printerID.toString());
-        result(null, newAccount);
+        result(res);
     });
   }
 
@@ -46,11 +46,11 @@ class ManageModel {
     db.query(`UPDATE printer SET status = 1 WHERE printer_id = '${printerID}'`, (err, res) => {
         if (err) {
             console.log("Error when enabling printer: ", err);
-            result(err, null);
+            result(null);
             return;
         }
         console.log("Enabled printer with ID:"+printerID.toString());
-        result(null, newAccount);
+        result(res);
     });
   }
 
@@ -59,31 +59,25 @@ class ManageModel {
     db.query(`UPDATE printer SET status = 0 WHERE printer_id = '${printerID}'`, (err, res) => {
         if (err) {
             console.log("Error when disabling printer: ", err);
-            result(err, null);
+            result(null);
             return;
         }
         console.log("Disabled printer with ID:"+printerID.toString());
-        result(null, newAccount);
+        result(res);
     });
   }
 
 // Update printer
-  
-
-
-}
-
-// Maybe not needed
-ManageModel.add = (newPrinter, result) => {
-    db.query("INSERT INTO printer SET ?", newPrinter, (err, res) => {
+  update_printer(newPrinter,result) {
+    db.query("UPDATE printer SET ? WHERE ?", [newPrinter, newPrinter.printer_id], (err, res) => {
         if (err) {
-            console.log("error: ", err);
-            result(err, null);
+            console.log("Error when updating printer: ", err);
+            result(null);
             return;
         }
-        console.log("Added printer :", { id: res.insertId, ...newAccount });
-        result(null, newAccount);
+        console.log("Updated printer :", { id: res.printer_id, ...newPrinter });
+        result(res);
     });
-};
-
+  }
+}
 module.exports = new ManageModel();
