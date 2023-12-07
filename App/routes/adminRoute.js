e6ab39124fe36cec58c058ae3b5d1ddcccc325a4
MarkStanly <nhien.nguyenroad2mag80@hcmut.edu.vn>
2023-12-07 11:59:48 +0700

    added manage routes

--- App/routes/adminRoute.js
+++ App/routes/adminRoute.js
@@ -1,20 +1,19 @@
 const express = require("express");
 const configurationController = require("../controllers/configuration.controller");
 const historyController = require("../controllers/history.controller");
-
+const manageController = require("../controllers/manage.controller")
 const router = express.Router();
 
 // api -> controller
 
 //nhien lam
 // Show chuc nang ra
-router.post("/manage", (req, res) => {});
-router.post("/manage/add", (req, res) => {});
-router.post("/manage/update", (req, res) => {});
-router.post("/manage/delete", (req, res) => {});
-// Co the gop chung
-router.post("/manage/enable", (req, res) => {});
-router.post("/manage/disable", (req, res) => {});
+router.post("/manage", (req, res) => {manageController.manage_get_printers(req,res)});
+router.post("/manage/add", (req, res) => {manageController.manage_add_printer(req,res)});
+router.post("/manage/update", (req, res) => {manageController.manage_update_printer(req,res)});
+router.post("/manage/delete", (req, res) => {manageController.manage_delete_printer(req,res)});
+router.post("/manage/enable", (req, res) => {manageController.manage_enable_printer(req,res)});
+router.post("/manage/disable", (req, res) => {manageController.manage_disable_printer(req,res)});
 
 // configure system ->
 router.post("/configuration", configurationController.config);
